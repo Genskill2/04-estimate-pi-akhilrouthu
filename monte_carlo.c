ebc2856aa4d2c2396415fc67adbfcb85c9816d04
akhilrouthu <85018230+akhilrouthu@users.noreply.github.com>
2021-06-03 17:02:16 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -5,20 +5,18 @@
 
 float mc_pi(int i)
 {
-  float x,y,b,e; 
-  
-float a=0; 
-for (int n=0;n<=i;n++){
-float x = frandom(); 
-float y = frandom();
-  
-float b = sqrt((x*x)+(y*y));
-if(b<=1){ 
-   a++; } 
-} 
-e=(4*a)/i ; 
-return(e);
-  
+  float x,y,b,e;
+  float a = ;
+  for(int n = 0; n<=i; n++)
+  {
+    x = frandom();
+    y = frandom();
+    b = sqrt((x*x) + (y*y));
+    if(b<=1)
+      a++;
+  }
+  e = (4*a)/i;
+  return e;
   
 }
 
